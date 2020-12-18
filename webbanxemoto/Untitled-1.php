b1314685ed81267ccfbc43562062ba741cd49a52
huutri <kariskylnzytis@gmail.com>
2020-12-18 22:40:28 +0700

    them folder web

---
+++ webbanxemoto/Untitled-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="stye/css.css" />
+<title>Web site bán xe moto</title>
+</head>
+
+<body>
+<div class="wrapper">
+	<div class="header"></div>
+    <div class="menu"></div>
+    <div class="content"></div>
+    <div class="footer"></div>
+</div>
+</body>
+</html>
